2bb2ec454197c29c1e0eace591b79827f1c46f58
russell <russell@voomify.com>
2011-03-10 10:58:45 -0500

    added imagemagick

---
+++ cookbooks/image_magick/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Voomify, LLC"
+maintainer_email "russell@voomify.com"
+license          "All rights reserved"
+description      "Installs/Configures image_magick"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
